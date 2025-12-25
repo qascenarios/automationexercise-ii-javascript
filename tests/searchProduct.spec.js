10d74786047d1be492224b780ae00e25705d0d56
Sulaimon Ekundayo <topdandydot@gmail.com>
2025-12-26 00:32:58 +0200

    Reposition dialog handling

--- tests/searchProduct.spec.js
+++ tests/searchProduct.spec.js
@@ -6,10 +6,10 @@ test('Product search flow', async ({ page }) => {
   const searchProductPage = new SearchProductPage(page);
 
   await openUrl(page, '/products');
-  await searchProductPage.handleDialog();
-
+  
   const searchInput = ['Blue Top', 'Stylish Dress', 'Fancy Green Top'];
   for (const term of searchInput) {
+    await searchProductPage.handleDialog();
     await searchProductPage.enterSearchProduct(term);
     await searchProductPage.clickSearch();
     
